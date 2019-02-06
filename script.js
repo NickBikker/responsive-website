29a11a31324196af79f01b5650b20cefd12a2405
Nick Bikker <nickbikkerprive@gmail.com>
2019-02-06 14:36:00 +0100

    responsive website done

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 function drop1() {
     document.getElementById('dropdown1').classList.toggle('show');
 }
+
+function drop2 () {
+    document.getElementById('dropdown2').classList.toggle('show');
+}
